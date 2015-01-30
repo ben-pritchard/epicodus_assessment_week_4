6dba6bc3be204604aa1063ec8adf6dbdb21f24c0
Ben Pritchard <ben.s.pritchard@gmail.com>
2015-01-30 12:05:28 -0800

    Add view band, edit band, and delete band functionality.

--- app.rb
+++ app.rb
@@ -7,9 +7,14 @@ get("/") do
   erb(:index)
 end
 
-post("/bands") do
-  name = params.fetch("name")
-  @band = Band.create({:name => name})
+get("/bands/:id") do
+  @band = Band.find(params.fetch("id").to_i())
+  erb(:band)
+end
+
+get("/edit/:id") do
+  id = params.fetch("id").to_i()
+  @band = Band.find(id)
   erb(:edit_band)
 end
 
@@ -17,5 +22,21 @@ patch("/update") do
   id = params.fetch("id").to_i()
   name = params.fetch("name")
   Band.find(id).update({:name => name})
+  redirect("/")
+end
+
+get("/delete/:id") do
+  id = params.fetch("id").to_i()
+  Band.find(id).destroy()
+  redirect("/")
+end
+
+get("/bands") do
+  erb(:add_band)
+end
+
+post("/bands") do
+  name = params.fetch("name")
+  Band.create({:name => name})
   redirect("/")
 end
